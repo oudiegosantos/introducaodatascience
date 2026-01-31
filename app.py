14a8676698948f06c6df207d1a26495443cee673
oudiegosantos <diegossmontenegro@gmail.com>
2026-01-31 17:27:23 -0300

    Lendo arquivos e extraindo alguns dados

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
-nome = "Diego"
-print(nome)
+import pandas as pd
+
+notas = pd.read_csv("arquivos/ratings.csv")
+
+notas.columns = "usuarioId", "filemID", "nota", "momento"
+print(notas["nota"].unique())
+print(notas["nota"].value_counts())
+
