a938beddee146b9577fa7aa36b46164568fd115d
oudiegosantos <diegossmontenegro@gmail.com>
2026-01-31 18:17:19 -0300

    Gerando gráficos do tipo hist e boxsplot utilizando libs como seaborn e matplotlib

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 notas = pd.read_csv("arquivos/ratings.csv")
 
 notas.columns = "usuarioId", "filemID", "nota", "momento"
 print(notas["nota"].unique())
 print(notas["nota"].value_counts())
+
+notas["nota"].plot(kind='hist')
+plt.show()
+
+media = notas["nota"].mean()
+mediana = notas["nota"].median()
+
+print(f"Média: {media}")
+print(f"Mediana: {mediana}")
+print(notas["nota"].describe())
+
+sns.boxplot(notas["nota"])
+plt.show()
 
